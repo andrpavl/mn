936e1adc1902622f251d6513ee8d61d894b532e1
Andrii Pavliuk <puvlo@yahoo.com>
2025-11-07 22:27:00 +0100

    refactor: migrate styles to CSS modules for App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import FundraisingList from "./components/Fundraising/Fundraising";
 import NeedsList from "./components/NeedsList/NeedsList";
 import Streams from "./components/Streams/Streams";
+import styles from "./App.module.css";
 
 function App() {
 	return (
-		<div className="App">
+		<div className={styles.App}>
 			<NeedsList />
-			<div className="adds">
+			<div className={styles.adds}>
 				<FundraisingList />
 				<Streams />
 			</div>
